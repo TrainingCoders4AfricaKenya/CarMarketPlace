3391850027d7c21e1fec512aa3dfda4f65428326
barney <https://github.com/3barney>
2013-06-27 12:20:01 +0300

    contains template style used for rendering content on the view files (header, body, footer)

---
+++ application/views/includes/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset= UTF-8">
+		<title>Benin Car Market Place</title>
+		<link rel="Stylesheet" href="<?php echo base_url();?>/css/style.css" type="text/css" media="screen" >
+	</head>
+	<body>
+	
